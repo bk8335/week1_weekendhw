799c08b4919db17bbbfaa3d4c463e4f81cfcc5d2
Benjamin <benjamin.kinnard1@gmail.com>
2017-03-10 15:57:22 +0000

    completed second integration test

--- pet_shop.rb
+++ pet_shop.rb
@@ -71,7 +71,11 @@ def customer_can_afford_pet(customer, new_pet)
 end
 
 def sell_pet_to_customer(pet_shop, pet, customer)
-  customer[:pets].push(pet)
-  pet_shop[:admin][:pets_sold] += 1
-  pet_shop[:admin][:total_cash] += pet[:price]
+  if pet == nil
+    return nil
+  end    
+      customer[:pets].push(pet)
+      pet_shop[:admin][:pets_sold] += 1
+      pet_shop[:admin][:total_cash] += pet[:price]
+    
 end
